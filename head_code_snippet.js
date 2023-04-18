db34df6da642437883603875edd0446655a011b2
Krishna Hawk <95653379+krishnahawk@users.noreply.github.com>
2023-04-18 09:32:19 -1000

    Customizing uscreen signup flow injecting content

--- head_code_snippet.js
+++ head_code_snippet.js
@@ -1,47 +1,47 @@
 let detectCustomerInfo = () => {
-    if (location.href.includes('/customer_info')) {
-      let wrapper = document.querySelector('.card .description')
-      if (wrapper) {
-        if (!wrapper.classList.value.includes('touched')) {
-          let mobileMarkup = `
-            <img class="mobile-customer-infor-footer" src="https://s3.amazonaws.com/unode1/assets/11794/ifP8NKIqQjqQ1TqWj8sN_mobile_customer_info.svg">
-          `;
-          let markup = `
-            <div class="customer-info-footer">
-              <h2 class="customer-info-footer--title">My youngest son developed perfect pitch before he was age 4 by using Mr. Rob's lessons! He is 5 now, and loves to point what notes his Dad is playing on the guitar.</h2>
-              <img src="https://s3.amazonaws.com/unode1/assets/11794/x23b3OeQnCsWEKnj9uQe_customer_info_footer.svg">
-            </div>
-          `
-          wrapper.insertAdjacentHTML('beforeEnd', mobileMarkup)
-          wrapper.insertAdjacentHTML('afterEnd', markup)
-          wrapper.classList.add('touched')
-        }
+  if (location.href.includes('/customer_info')) {
+    let wrapper = document.querySelector('.card .description')
+    if (wrapper) {
+      if (!wrapper.classList.value.includes('touched')) {
+        let mobileMarkup = `
+          <mobile-customer-infor-footer" src="https://s3.amazonaws.com/unode1/assets/11794/ifP8NKIqQjqQ1TqWj8sN_mobile_customer_info.svg">
+        `;
+        let markup = `
+          <div class="customer-info-footer">
+            <h2 class="customer-info-footer--title">My youngest son developed perfect pitch before he was age 4 by using Mr. Rob's lessons! He is 5 now, and loves to point what notes his Dad is playing on the guitar.</h2>
+            <img src="https://s3.amazonaws.com/unode1/assets/11794/x23b3OeQnCsWEKnj9uQe_customer_info_footer.svg">
+          </div>
+        `
+        wrapper.insertAdjacentHTML('beforeEnd', mobileMarkup)
+        wrapper.insertAdjacentHTML('afterEnd', markup)
+        wrapper.classList.add('touched')
       }
     }
-    setTimeout(() => detectCustomerInfo(), 500) 
   }
-  let detectCompleteOrder = () => {
-    if (location.href.includes('/complete_order')) {
-      let wrapper = document.querySelector('.card .description')
-      if (wrapper) {
-        if (!wrapper.classList.value.includes('touched')) {
-          let mobileMarkup = `
-            <img class="mobile-customer-infor-footer" src="https://s3.amazonaws.com/unode1/assets/11794/ifP8NKIqQjqQ1TqWj8sN_mobile_customer_info.svg">
-          `;
-          let markup = `
-            <div class="customer-info-footer">
-              <h2 class="customer-info-footer--title">My youngest son developed perfect pitch before he was age 4 by using Mr. Rob's lessons! He is 5 now, and loves to point what notes his Dad is playing on the guitar.</h2>
-              <img src="https://s3.amazonaws.com/unode1/assets/11794/x23b3OeQnCsWEKnj9uQe_customer_info_footer.svg">
-            </div>
-          `
-          wrapper.insertAdjacentHTML('beforeEnd', mobileMarkup)
-          wrapper.insertAdjacentHTML('afterEnd', markup)
-          wrapper.classList.add('touched')
-        }
+  setTimeout(() => detectCustomerInfo(), 500) 
+}
+let detectCompleteOrder = () => {
+  if (location.href.includes('/complete_order')) {
+    let wrapper = document.querySelector('.card .description')
+    if (wrapper) {
+      if (!wrapper.classList.value.includes('touched')) {
+        let mobileMarkup = `
+          <img class="mobile-customer-infor-footer" src="https://s3.amazonaws.com/unode1/assets/11794/ifP8NKIqQjqQ1TqWj8sN_mobile_customer_info.svg">
+        `;
+        let markup = `
+          <div class="customer-info-footer">
+            <h2 class="customer-info-footer--title">My youngest son developed perfect pitch before he was age 4 by using Mr. Rob's lessons! He is 5 now, and loves to point what notes his Dad is playing on the guitar.</h2>
+            <img src="https://s3.amazonaws.com/unode1/assets/11794/x23b3OeQnCsWEKnj9uQe_customer_info_footer.svg">
+          </div>
+        `
+        wrapper.insertAdjacentHTML('beforeEnd', mobileMarkup)
+        wrapper.insertAdjacentHTML('afterEnd', markup)
+        wrapper.classList.add('touched')
       }
     }
-    setTimeout(() => detectCompleteOrder(), 500) 
   }
-  
-  detectCompleteOrder()
-  detectCustomerInfo()
+  setTimeout(() => detectCompleteOrder(), 500) 
+}
+
+detectCompleteOrder()
+detectCustomerInfo()
